876ddc77c8abec656495f46197949034177c6905
giorgiocaligiuri <giorgio.caligiuri7@gmail.com>
2023-12-12 21:39:47 +0100

    primo

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const validateNumber = function (number) {
+    //check if null or empty
+    if (number === "" || number === null) {
+        return false;
+    }
+
+    //check if number
+    if (isNaN(number)) {
+        return false;
+    }
+
+    return true;
+}
+
+const calcolaNettoDaImponibile = function (imponibile) {
+    const scaglioni = [ 15000, 28000, 55000, 75000, 100000 ];
+    const aliquote = [ 23, 27, 38, 41, 43 ];
+    
+    let totaleTasse = 0;
+    let imponibile_rimanente = imponibile;
+    let i = 0;
+    while (imponibile_rimanente > 0) {
+        const scaglione = scaglioni[i];
+        const aliquota = aliquote[i];
+        const imponibile_scaglione = Math.min(imponibile_rimanente, scaglione);
+        const tassa_scaglione = imponibile_scaglione * aliquota / 100;
+        totaleTasse += tassa_scaglione;
+        imponibile_rimanente -= imponibile_scaglione;
+        i++;
+    }
+
+    return imponibile - totaleTasse;
+}
+
+const btn_calcola_click_eventHandeler = function () {
+    //get value from input with id "txt_imponibile"
+    const input_text = document.getElementById("txt_imponibile").value;
+
+    //validate input
+    if (!validateNumber(input_text)) {
+        alert("Inserisci un numero");
+        return;
+    }
+
+    //convert input to number
+    const imponibile = Number(input_text);
+
+    //calculate tax
+    const netto = calcolaNettoDaImponibile(imponibile);
+
+    //show result
+    document.getElementById("stipendio_netto").innerHTML = `stipendio netto (no tredicesiama) ${netto / 12} € al mese\n
+    stipendio netto (con tredicesima) ${netto / 13} € all'anno`
+
+};
+
+
+//add listener to button click, button with id "btn_calcola"
+document.getElementById("btn_calcola").addEventListener("click", btn_calcola_click_eventHandeler);
+
