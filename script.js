2767e64bfff075bd935e4b01e7d9c7b807fd14d4
giorgiocaligiuri <giorgio.caligiuri7@gmail.com>
2023-12-13 00:04:36 +0100

    other style fixes

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const calcolaNettoDaImponibile = function (imponibile) {
         totaleTasse += tassa_scaglione;
         imponibile_rimanente -= imponibile_scaglione;
         //add values to table
-        insertTableRow(`${i > 0 ? scaglioni[i-1] : "0"} - ${scaglione}`, `${aliquota} %`, imponibile_scaglione, tassa_scaglione);
+        insertTableRow(`${i > 0 ? scaglioni[i-1] : "0"} - ${scaglione}`, `${aliquota}`, imponibile_scaglione, tassa_scaglione);
     }
     // apply last aliquota
     if (imponibile_rimanente > 0) {
@@ -57,7 +57,7 @@ const calcolaNettoDaImponibile = function (imponibile) {
         const tassa_scaglione = imponibile_rimanente * aliquota / 100;
         totaleTasse += tassa_scaglione;
         //add values to table
-        insertTableRow(`${scaglioni[scaglioni.length - 1]} - `, `${aliquota} %`, imponibile_rimanente, tassa_scaglione);
+        insertTableRow(`${scaglioni[scaglioni.length - 1]} - `, `${aliquota}`, imponibile_rimanente, tassa_scaglione);
     }
 
 
@@ -67,6 +67,34 @@ const calcolaNettoDaImponibile = function (imponibile) {
 const txt_imponebile_textChanged_eventHandeler = function () {
     //get value from input with id "txt_imponibile"
     const input_text = document.getElementById("txt_imponibile").value;
+
+    if (input_text === "") {
+      //empty input
+      //hide error message
+      document.getElementById("error").classList.remove("visible");
+      document.getElementById("error").classList.add("hidden");
+      //hide table
+      document.getElementById("table").classList.remove("visible");
+      document.getElementById("table").classList.add("hidden");
+      // hide p_stipendio_netto_no_tred
+      document.getElementById("p_stipendio_netto_no_tred").classList.remove("visible");
+      document.getElementById("p_stipendio_netto_no_tred").classList.add("hidden");
+      // hide p_stipendio_netto_tred
+      document.getElementById("p_stipendio_netto_tred").classList.remove("visible");
+      document.getElementById("p_stipendio_netto_tred").classList.add("hidden");
+      return;
+    }
+    // text not empty
+    //show table
+    document.getElementById("table").classList.remove("hidden");
+    document.getElementById("table").classList.add("visible");
+    // show p_stipendio_netto_no_tred
+    document.getElementById("p_stipendio_netto_no_tred").classList.remove("hidden");
+    document.getElementById("p_stipendio_netto_no_tred").classList.add("visible");
+    // show p_stipendio_netto_tred
+    document.getElementById("p_stipendio_netto_tred").classList.remove("hidden");
+    document.getElementById("p_stipendio_netto_tred").classList.add("visible");
+
 
     //validate input
     if (!validateNumber(input_text)) {
@@ -94,8 +122,8 @@ const txt_imponebile_textChanged_eventHandeler = function () {
     
 
     //show result
-    document.getElementById("stipendio_netto_no_tred").innerHTML = `stipendio netto (no tredicesima) : ${stipendio_netto_no_tred} € al mese`
-    document.getElementById("stipendio_netto_tred").innerHTML = `stipendio netto (con tredicesima) : ${stipendio_netto_tred} € al mese`
+    document.getElementById("p_stipendio_netto_no_tred").innerHTML = `• stipendio netto (no tredicesima) : ${stipendio_netto_no_tred} € al mese`
+    document.getElementById("p_stipendio_netto_tred").innerHTML = `• stipendio netto (con tredicesima) : ${stipendio_netto_tred} € al mese`
 
 };
 
